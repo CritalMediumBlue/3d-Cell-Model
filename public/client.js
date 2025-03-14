3dbf016d365348b530aeb2cb7ec53dff5ef1d585
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-03-14 14:50:18 +0100

    Rewriting some things

--- public/client.js
+++ public/client.js
@@ -3,146 +3,51 @@ import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-
-let WIDTH = window.innerWidth;
-let HEIGHT = window.innerHeight;
-
-var keyLight, fillLight, backLight;
-var scene, camera, renderer, cameraControls;
-
-var cellParts = new Array();
-
-// Function in charge to create and set up imporant components of the scene.
-function init(){
-
-
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(50, WIDTH / HEIGHT, 1, 1000);
-    renderer = new THREE.WebGLRenderer({antialias: true});
-
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(WIDTH, HEIGHT);
-    renderer.setClearColor(0x000000, 1);
-    document.body.appendChild(renderer.domElement);
-
-    loadObject();
-    lightingSetUp();
-
-    camera.position.set(40, 40, 40);
-    camera.lookAt(new THREE.Vector3(0,0,0));
-
-    renderer.render(scene, camera);
-}
-
-// It loads major components of the cell object.
-function loadObject(){
-
-    var objLoader = new OBJLoader();
-    var mtlLoader = new MTLLoader();
-
-    mtlLoader.setPath('./cellModel/');
-    mtlLoader.load("CellAnatomy.mtl", function (mtls){
-
-    mtls.preload();
-
-    objLoader.setMaterials(mtls);
-    objLoader.setPath("./cellModel/");
-
-    objLoader.load("CellAnatomy.obj", function (obj){
-
-        obj.scale.set(0.5,0.5,0.5);
-        obj.name = "cellObject";
-
-    
-        scene.add(obj);
-
-        // Stores all of the objects from the scene in an array that will be used later
-        // for interactions.
-        scene.traverse(function(children){
-            cellParts.push(children);
-        });
-
-        renderer.render(scene, camera);
-        },
-        function (xhr){
-
-        if (xhr.lengthComputable) {
-
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log( 'cell ' + Math.round( percentComplete, 2 ) + '% downloaded' );
-
-            if (Math.round( percentComplete, 2 ) == 100)
-            {
-                var test = document.getElementsByClassName("loader")[0];
-                test.style.visibility = "hidden";
-            }
-            
-        }
-        }, function (){});
-    });
-}
-
-// Set ups the lighting in the scene.
-function lightingSetUp(){
-
-    keyLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+class CellViewer {
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);    
+    this.camera.position.set(40, 40, 40);
+    this.camera.lookAt(0, 0, 0);
+    const keyLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
     keyLight.position.set(3, 10, 3).normalize();
-    keyLight.name = "Light1";
-
-    fillLight = new THREE.DirectionalLight(0xFFFFFF, 1.2);
+    const fillLight = new THREE.DirectionalLight(0xFFFFFF, 1.2);
     fillLight.position.set(0, -5, -1).normalize();
-    fillLight.name = "Light2";
-
-    backLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+    const backLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
     backLight.position.set(-10, 0, 0).normalize();
-    backLight.name = "Light3";
-
-    scene.add(keyLight);
-    scene.add(fillLight);
-    scene.add(backLight);
-}
-
-
-
-// Event listener for window resize.
-function handleResize(){
-
-    window.addEventListener("resize", function (){
-
-    WIDTH = window.innerWidth;
-    HEIGHT = window.innerHeight;
-
-    renderer.setSize(WIDTH, HEIGHT);
-    camera.apsect = WIDTH / HEIGHT;
-    camera.updateProjectionMatrix();
-
-
-    renderer.render(scene, camera);
+    const fogColor = new THREE.Color(0x0F0000);  // Light gray fog
+    this.scene.fog = new THREE.Fog(fogColor, 10, 120);
+    this.scene.background = fogColor;
+    this.scene.add(keyLight, fillLight, backLight);   
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+ 
+    this.loadCellModel();
+    this.animate();
+  }
+  
+  loadCellModel() {
+    const mtlLoader = new MTLLoader();
+    mtlLoader.setPath('./cellModel/');
+    mtlLoader.load("CellAnatomy.mtl", (materials) => {
+      materials.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.setPath("./cellModel/");
+      objLoader.load("CellAnatomy.obj", (object) => {
+        object.scale.set(0.5, 0.5, 0.5);
+        this.scene.add(object);
+      });
     });
+  }
+  
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
 }
 
-// Enables orbital control.
-function orbitalControl(){
-
-    cameraControls = new OrbitControls(camera, renderer.domElement);
-    cameraControls.addEventListener("change", function(){
-
-    camera.updateProjectionMatrix();
-    renderer.render(scene, camera);
-
-    });
-}
-
-
-function animate(){
-
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-init();
-orbitalControl();
-handleResize();
-animate();
-
-//document.addEventListener("click", onDocumentMouseDown, false);
+document.addEventListener('DOMContentLoaded', () => new CellViewer());
