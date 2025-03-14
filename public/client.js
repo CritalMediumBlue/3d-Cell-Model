85b6577415833d55ec8594939c89c690d8443816
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-03-14 15:42:49 +0100

    finnished

--- public/client.js
+++ public/client.js
@@ -24,8 +24,16 @@ class CellViewer {
     this.scene.background = fogColor;
     this.scene.add(keyLight, fillLight, backLight);   
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
- 
+
+    this.proteins = []; // Array to store proteins
+    this.waterMolecules = []; // Array to store water molecules
+    this.proteinSpeed = 0.1; // Controls the speed of Brownian motion
+    this.waterSpeed = 1; // Controls the speed of Brownian motion
+    this.cellRadius = 25; // Radius of the cell membrane
+
     this.loadCellModel();
+    this.createParticles(); // Add proteins to the scene
+    //this.createCellMembrane(); // Add cell membrane to the scene
     this.animate();
   }
   
@@ -39,12 +47,95 @@ class CellViewer {
       objLoader.setPath("./cellModel/");
       objLoader.load("CellAnatomy.obj", (object) => {
         object.scale.set(0.5, 0.5, 0.5);
+        object.position.set(1, -26, 0);
         this.scene.add(object);
       });
     });
   }
+
+  createParticles() {
+    const proteinGeometry = new THREE.SphereGeometry(0.5, 4, 4);
+    const waterGeometry = new THREE.SphereGeometry(0.1, 1, 1);
+    
+    const proteinColor=0x00ff00;
+    const waterColor=0x0000ff;
+
+    
+    for (let i = 0; i < 300; i++) {
+      const proteinMaterial = new THREE.MeshPhongMaterial({ 
+        emissive: proteinColor,
+        emissiveIntensity: 1,
+      });
+      
+      const protein = new THREE.Mesh(proteinGeometry, proteinMaterial);
+      
+    
+      protein.position.set(
+        (Math.random() - 0.5)*50,
+        (Math.random() - 0.5)*50,
+        (Math.random() - 0.5)*50
+      );
+      
+      this.scene.add(protein);
+      this.proteins.push(protein);
+    }
+
+    for (let i = 0; i < 200; i++) {
+        const waterMaterial = new THREE.MeshPhongMaterial({ 
+            emissive: waterColor,
+            emissiveIntensity: 1,
+        });
+        const water = new THREE.Mesh(waterGeometry, waterMaterial);
+        water.position.set(
+            (Math.random() - 0.5) * 50,
+            (Math.random() - 0.5) * 50,
+            (Math.random() - 0.5) * 50
+        );
+        this.scene.add(water);
+        this.waterMolecules.push(water);
+        }
+  }
+
+  createCellMembrane() {
+    const segments = 64; // Number of segments
+    const geometry = new THREE.SphereGeometry(this.cellRadius, segments, segments);
+    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.position.set(0, 0, 0);
+    this.scene.add(sphere);
+  }
   
   animate() {
+    // Update protein positions with Brownian motion
+    this.proteins.forEach(protein => {
+      // Random movement in each direction
+      protein.position.x += (Math.random() - 0.5) * this.proteinSpeed;
+      protein.position.y += (Math.random() - 0.5) * this.proteinSpeed;
+      protein.position.z += (Math.random() - 0.5) * this.proteinSpeed;
+
+    //if the position is outside the bounds of the cell membrane, reflect the protein back into the cell
+    const radius = this.cellRadius;
+    if (protein.position.length() > radius) {
+            protein.position.setLength(radius);
+           }
+      
+    });
+
+    // Update water molecule positions with Brownian motion
+    this.waterMolecules.forEach(water => {
+      // Random movement in each direction
+      water.position.x += (Math.random() - 0.5) * this.waterSpeed;
+      water.position.y += (Math.random() - 0.5) * this.waterSpeed;
+      water.position.z += (Math.random() - 0.5) * this.waterSpeed;
+
+        //if the position is outside the bounds of the cell membrane, reflect the protein back into the cell
+        const radius = this.cellRadius;
+        if (water.position.length() > radius) {
+            water.position.setLength(radius);
+        }
+    }
+    );
+    
     requestAnimationFrame(this.animate.bind(this));
     this.renderer.render(this.scene, this.camera);
   }
