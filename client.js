9f4dbc2ecff6fb85abd5402a57d23fbf7438607d
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-03-14 16:29:16 +0100

    VR

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 class CellViewer {
   constructor() {
@@ -10,33 +11,44 @@ class CellViewer {
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);    
+    const meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, initial-scale=1.0, user-scalable=no';
+    document.head.appendChild(meta);
+    // Enable WebXR
+    this.renderer.xr.enabled = true;
+    document.body.appendChild(this.renderer.domElement);
+    
+    // Add VR button to the page
+    document.body.appendChild(VRButton.createButton(this.renderer));  
     this.camera.position.set(40, 40, 40);
     this.camera.lookAt(0, 0, 0);
-    const keyLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    const keyLight = new THREE.DirectionalLight(0xFFFFFF, 3);
     keyLight.position.set(3, 10, 3).normalize();
-    const fillLight = new THREE.DirectionalLight(0xFFFFFF, 1.2);
-    fillLight.position.set(0, -5, -1).normalize();
-    const backLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
-    backLight.position.set(-10, 0, 0).normalize();
-    const fogColor = new THREE.Color(0x0F0000);  // Light gray fog
-    this.scene.fog = new THREE.Fog(fogColor, 10, 120);
+    const fogColor = new THREE.Color(0x0F000F);  // Light gray fog
+    this.scene.fog = new THREE.Fog(fogColor, 2, 90);
     this.scene.background = fogColor;
-    this.scene.add(keyLight, fillLight, backLight);   
+    this.scene.add(keyLight);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-    this.proteins = []; // Array to store proteins
-    this.waterMolecules = []; // Array to store water molecules
-    this.proteinSpeed = 0.1; // Controls the speed of Brownian motion
-    this.waterSpeed = 1; // Controls the speed of Brownian motion
-    this.cellRadius = 25; // Radius of the cell membrane
+    this.proteins = []; 
+    this.waterMolecules = []; 
+    this.proteinSpeed = 0.1; 
+    this.waterSpeed = 1; 
+    this.cellRadius = 25; 
 
     this.loadCellModel();
-    this.createParticles(); // Add proteins to the scene
-    //this.createCellMembrane(); // Add cell membrane to the scene
+    this.createParticles(); 
     this.animate();
+ 
+    window.addEventListener('resize', this.onWindowResize.bind(this));
   }
   
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
   loadCellModel() {
     const mtlLoader = new MTLLoader();
     mtlLoader.setPath('./cellModel/');
@@ -59,17 +71,15 @@ class CellViewer {
     
     const proteinColor=0x00ff00;
     const waterColor=0x0000ff;
-
+    const proteinMaterial = new THREE.MeshPhongMaterial({ 
+      emissive: proteinColor,
+      emissiveIntensity: 1,
+    });
     
     for (let i = 0; i < 300; i++) {
-      const proteinMaterial = new THREE.MeshPhongMaterial({ 
-        emissive: proteinColor,
-        emissiveIntensity: 1,
-      });
-      
+     
       const protein = new THREE.Mesh(proteinGeometry, proteinMaterial);
       
-    
       protein.position.set(
         (Math.random() - 0.5)*50,
         (Math.random() - 0.5)*50,
@@ -79,12 +89,13 @@ class CellViewer {
       this.scene.add(protein);
       this.proteins.push(protein);
     }
+    const waterMaterial = new THREE.MeshPhongMaterial({ 
+      emissive: waterColor,
+      emissiveIntensity: 1,
+      });
 
     for (let i = 0; i < 200; i++) {
-        const waterMaterial = new THREE.MeshPhongMaterial({ 
-            emissive: waterColor,
-            emissiveIntensity: 1,
-        });
+        
         const water = new THREE.Mesh(waterGeometry, waterMaterial);
         water.position.set(
             (Math.random() - 0.5) * 50,
@@ -96,14 +107,7 @@ class CellViewer {
         }
   }
 
-  createCellMembrane() {
-    const segments = 64; // Number of segments
-    const geometry = new THREE.SphereGeometry(this.cellRadius, segments, segments);
-    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
-    const sphere = new THREE.Mesh(geometry, material);
-    sphere.position.set(0, 0, 0);
-    this.scene.add(sphere);
-  }
+
   
   animate() {
     // Update protein positions with Brownian motion
@@ -136,7 +140,7 @@ class CellViewer {
     }
     );
     
-    requestAnimationFrame(this.animate.bind(this));
+    this.renderer.setAnimationLoop(this.animate.bind(this));
     this.renderer.render(this.scene, this.camera);
   }
 }
