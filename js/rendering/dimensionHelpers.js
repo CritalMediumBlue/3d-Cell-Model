3a0817702b1b7031a5e35c6ed06593acd6bff9ef
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-09-18 17:17:01 +0200

    cdfdfd

--- js/rendering/dimensionHelpers.js
+++ js/rendering/dimensionHelpers.js
@@ -119,7 +119,7 @@ export class DimensionHelpers {
     const timeRate = (simulationTimeStep / deltaTime).toFixed(5);
 
     // Create new time label
-    this.currentTimeLabel = this.createTextLabel("Simulation speed: " + timeRate + "x real-time", 0x000000, 1, 5 * 256, 5 * 64);
+    this.currentTimeLabel = this.createTextLabel("Simulation speed: " + timeRate + "x real-time", 0x000000, 1.5,7 * 256, 7 * 64);
     this.currentTimeLabel.position.set(0, this.cellRadius + 1, 0);
     this.currentTimeStepLabel = this.createTextLabel("Simulation Step: " + simulationTimeStep.toFixed(5) + "s", 0x000000, 1, 5 * 256, 5 * 64);
     this.currentTimeStepLabel.position.set(0, this.cellRadius + 0.5, 0);
