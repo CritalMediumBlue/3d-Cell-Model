8b3b69a6651f6b0f8335af17618fdf9a4552e9c1
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-09-25 14:33:10 +0200

    Adjust reticle placement height for better model positioning in AR

--- js/ar/ARController.js
+++ js/ar/ARController.js
@@ -78,7 +78,7 @@ export class ARController {
     if (this.reticle.visible && !this.modelPlaced) {
       // Place the cell group at the reticle position
       this.wholeSceneGroup.position.setFromMatrixPosition(this.reticle.matrix);
-      this.wholeSceneGroup.position.y += 0.77
+      this.wholeSceneGroup.position.y += 1.5
       this.wholeSceneGroup.scale.set(0.1, 0.1, 0.1);
       //hide the reticle after placing the model
       this.reticle.visible = true;
