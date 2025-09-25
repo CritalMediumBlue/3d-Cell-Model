ec82e36f8fe3a7357111d233bd9e75b7d20429cd
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-09-25 14:28:38 +0200

    Add vertical grid helpers and labels for 10 μm scale in dimension helpers

--- js/rendering/dimensionHelpers.js
+++ js/rendering/dimensionHelpers.js
@@ -90,10 +90,17 @@ export class DimensionHelpers {
     gridHelperSmallVertical.position.set(0, 0, -15); // Position it at the back
     this.staticGroup.add(gridHelperSmallVertical);
 
+    // Add a larger vertical grid helper to represent the 10 μm scale
+    const gridHelperBigVertical = gridHelperBig.clone();
+    gridHelperBigVertical.rotation.x = Math.PI / 2; // Rotate to vertical
+    gridHelperBigVertical.position.set(0, 0, -15); // Position it at the back
+    this.staticGroup.add(gridHelperBigVertical);
+
     // Store references for TouchHandler compatibility
     this.wholeSceneGroup.gridHelperSmall = gridHelperSmall;
     this.wholeSceneGroup.gridHelperBig = gridHelperBig;
     this.wholeSceneGroup.gridHelperSmallVertical = gridHelperSmallVertical;
+    this.wholeSceneGroup.gridHelperBigVertical = gridHelperBigVertical;
 
     // Add labels to the grid helpers to indicate 1 μm steps and 10 μm steps
     for (let i = -15; i <= 15; i += 5) {
@@ -101,18 +108,34 @@ export class DimensionHelpers {
       const label1um = this.createTextLabel(
         label + " μm", 0x000000
       );
+      const label1umVertical = label1um.clone();
       label1um.position.set(
         i, -15, 0
       );
+      label1umVertical.position.set(
+        i, 0, -15
+      );
+
       this.staticGroup.add(label1um);
+      this.staticGroup.add(label1umVertical);
       if (i !== 0) { // Avoid duplicating the zero label
-        const labelNeg = this.createTextLabel(
+        const label1um2 = this.createTextLabel(
           (-i) + " μm", 0x000000
         );
-        labelNeg.position.set(
+        const label1umVertical2 = label1um2.clone();
+        label1umVertical2.position.set(
           0, -15, i
         );
-        this.staticGroup.add(labelNeg);
+        this.staticGroup.add(label1umVertical2);
+        label1um2.position.set(
+          0, -15, i
+        );
+        this.staticGroup.add(label1um2);
+        const labelNegVertical = label1um2.clone();
+        labelNegVertical.position.set(
+         0, -i, -15
+        );
+        this.staticGroup.add(labelNegVertical);
       }
     }
     
