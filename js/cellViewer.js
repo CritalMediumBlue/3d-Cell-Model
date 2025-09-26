1f65a3d82794892e44859f2ab04f72a13a3858e1
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-09-26 15:32:20 +0200

    Adjust ATP molecule motion parameters for enhanced simulation accuracy

--- js/cellViewer.js
+++ js/cellViewer.js
@@ -160,7 +160,7 @@ export class CellViewer {
     
     if (!this.isPaused) {
       if(this.mode === "atp"){
-      this.brownianMotion.applyBrownianMotion(this.ATPSD, this.atpMolecules, 0, this.cellRadius*30, -1000,-15);
+      this.brownianMotion.applyBrownianMotion(this.ATPSD, this.atpMolecules, 0, this.cellRadius*100, -100000,-15);
       }
       if(this.mode === "cell"){
       this.brownianMotion.applyBrownianMotion(this.virusSD, this.viralParticles, this.cellRadius, this.cellRadius*2);
