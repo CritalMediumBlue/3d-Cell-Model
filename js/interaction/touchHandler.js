a40af6aead539cf796f4604b96dc573a7c802de3
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-09-26 11:06:31 +0200

    Prevent negative scaling and adjust visibility threshold for the helper grid

--- js/interaction/touchHandler.js
+++ js/interaction/touchHandler.js
@@ -103,7 +103,7 @@ export class TouchHandler {
           if (this.currentScale <= 0) {
             this.currentScale = 0; // Prevent scaling to negative values
           }
-          if (this.currentScale > 0.1) {
+          if (this.currentScale > 0.01) {
             // Hide the huge helper grid
             this.wholeSceneGroup.gridHelperHuge.visible = false;
           }
